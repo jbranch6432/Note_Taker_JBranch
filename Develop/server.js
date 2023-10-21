51991f3f6235657fba33ae7f33f0842866c725b1
Jamie Branch <schwung988@yahoo.com>
2023-10-20 20:22:24 -0400

    add routes

--- Develop/server.js
+++ Develop/server.js
@@ -12,10 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// //API Routes
 app.get('/', (req, res) =>
     res.sendFile(path.join(_dirname, '/public/index.html'))
 );
 
+app.get('/notes', (req, res) {
+    res.sendfile(path.join(__dirname, 'public/index.html'));
+
+});
+
+app.get('/api/notes', (req, res) {
+    return res.sendFile(path.json(__dirname, 'db/db.json'));
+})
 
 app.listen(3000, () => console.log(`Server listening at http://localhost:${PORT}`)
 );
