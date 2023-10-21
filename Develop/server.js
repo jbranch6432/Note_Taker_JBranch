5665e542f81b030780ae49da9324ad42beeafa99
Jamie Branch <schwung988@yahoo.com>
2023-10-21 00:00:05 -0400

    uuid.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-//Middleware: body parsing
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 // //API Routes
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
     res.sendFile(path.join(_dirname, '/public/index.html'))
 );
 
-app.get('/notes', (req, res) {
-    res.sendfile(path.join(__dirname, 'public/index.html'));
+app.get('/notes', (req, res) => {
+    res.sendfile(path.join(__dirname, 'public/notes.html'));
 
 });
 
-app.get('/api/notes', (req, res) {
+app.get('/api/notes', (req, res) => {
     return res.sendFile(path.json(__dirname, 'db/db.json'));
 })
 
+app.post('/api/notes', (req, res))
 app.listen(3000, () => console.log(`Server listening at http://localhost:${PORT}`)
 );
